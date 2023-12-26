0a512a10ab09c22f8bb4b0f4e2b834c5fbcdb8de
Valerii Rodtsevych <153345246+CaveManofTheFuture@users.noreply.github.com>
2023-12-25 21:29:24 -0800

    Rewrite script.js from scratch && finish the game

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 "use strict";
-function getComputerChoice(){
- let choices = ["Rock", "Paper", "Scissors"]; 
- let choice = choices[Math.floor(Math.random()*choices.length)];
- console.log("Comoputer chose " + choice);
+
+let userChoice = prompt("Rock? Paper? Scissors");
+userChoice = userChoice.toLowerCase();
+let choices = ["rock", "paper", "scissors"]; 
+let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+function getResult(){
+ //Comparing user and computer
+ // rock case
+   if (userChoice == "rock" && computerChoice == "paper"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("You win") 
+ } else if (userChoice == "rock" && computerChoice == "scissors"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("You win") 
+ } else if (userChoice == "rock" && computerChoice == "rock"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("Nobody wins") 
+ // scissors case  
+ } else if (userChoice == "scissors" && computerChoice == "paper"){
+ console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+ console.log("You win") 
+ } else if (userChoice == "scissors" && computerChoice == "scissors"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("Nobody wins") 
+ } else if (userChoice == "scissors" && computerChoice == "rock"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("You lose") 
+ } 
+ // rock case
+   else if (userChoice == "rock" && computerChoice == "paper"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("You lose") 
+ } else if (userChoice == "rock" && computerChoice == "scissors"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("You win") 
+ } else if (userChoice == "scissors" && computerChoice == "rock"){
+  console.log("Your choice was " + userChoice + " and computer's choice was " + computerChoice);
+  console.log("Nobody wins") 
+ } else {
+  console.log("Choose only rock/paper/scissors");
+ }
 }
 
-getComputerChoice();
+getResult();
+
+
+
+
+
